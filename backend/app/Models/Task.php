e21888994058f5c4f79fd96bd90dbb30c867b276
hatpoppo <toshi.hat@gmail.com>
2021-07-13 13:47:55 +0900

    日付の表示形式を変更する

--- backend/app/Models/Task.php
+++ backend/app/Models/Task.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Task extends Model
 {
@@ -42,5 +43,13 @@ class Task extends Model
             return '';
         }
         return self::STATUS[$status]['class'];
+    }
+    /**
+     * 整形した期限日
+     * @return string
+     */
+    public function getFormattedDueDateAttribute()
+    {
+        return Carbon::createFromFormat('Y-m-d',$this->attributes['due_date'])->format('Y/m/d');
     }
 }
