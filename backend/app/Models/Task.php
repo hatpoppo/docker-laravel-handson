010bbe6c932d4e96d4927f3eb872a45b6f4378a1
hatpoppo <toshi.hat@gmail.com>
2021-07-13 12:12:34 +0900

    Taskモデルにアクセサを追加

--- backend/app/Models/Task.php
+++ backend/app/Models/Task.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+    /**
+     * 状態定義
+     */
+    const STATUS = [
+        1 => ['label' => '未着手'],
+        2 => ['label' => '着手中'],
+        3 => ['label' => '完了'],
+    ];
+    /**
+     * 状態のラベル
+     * @return string
+     */
+    public function getStatusLabelAttribute()
+    {
+        $status = $this->attributes['status'];
+
+        //定義されていなければ空文字を返す
+        if (!isset(self::STATUS[$status])){
+            return '';
+        }
+        return self::STATUS[$status]['label'];
+    }
 }
