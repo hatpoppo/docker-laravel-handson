9262c217f4cc1583be3bc975c60c8cd7604c5989
hatpoppo <toshi.hat@gmail.com>
2021-07-19 16:36:18 +0900

    ユーザに関連するフォルダがあればタスク一覧を表示する

--- backend/app/Http/Controllers/HomeController.php
+++ backend/app/Http/Controllers/HomeController.php
@@ -9,6 +9,18 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        //ログインユーザを取得
+        $user = Auth::user();
+        //ログインユーザに関連するフォルダを取得する
+        $folder = $user->folders()->first();
+        //フォルダが見つからない場合ホームページを表示
+        if(is_null($folder)){
+            return view('home');
+        }
+        //フォルダがあればタスク一覧を表示
+        
+        return redirect()->route('tasks.index',[
+            'id' => $folder->id,
+        ]);
     }
 }
