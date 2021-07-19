b864865cf8c1f076f833fb18e0ae8104e055fe99
hatpoppo <toshi.hat@gmail.com>
2021-07-19 16:17:25 +0900

    ログイン、ログアウト、認証処理の組み込み

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,20 +14,21 @@ use App\Http\Controllers\HomeController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::group(['middleware' => 'auth'], function(){
+  Route::get('/', [HomeController::class, 'index'])->name('home');
+
+  Route::get('/folders/{id}/tasks',[TaskController::class, 'index'])->name('tasks.index');
+
+  Route::get('/folders/create',[FolderController::class, 'showCreateForm'])->name('folders.create');
+  Route::post('/folders/create',[FolderController::class, 'create']);
+
+  Route::get('/folders/{id}/tasks/create',[TaskController::class, 'showCreateForm'])->name('tasks.create');
+  Route::post('/folders/{id}/tasks/create',[TaskController::class, 'create']);
+
+  Route::get('/folders/{id}/tasks/{task_id}/edit',[TaskController::class, 'showEditForm'])->name('tasks.edit');
+  Route::post('/folders/{id}/tasks/{task_id}/edit',[TaskController::class, 'edit']);
+});
+
 Auth::routes();
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-
-Route::get('/folders/{id}/tasks',[TaskController::class, 'index'])->name('tasks.index');
-
-Route::get('/folders/create',[FolderController::class, 'showCreateForm'])->name('folders.create');
-Route::post('/folders/create',[FolderController::class, 'create']);
-
-Route::get('/folders/{id}/tasks/create',[TaskController::class, 'showCreateForm'])->name('tasks.create');
-Route::post('/folders/{id}/tasks/create',[TaskController::class, 'create']);
-
-Route::get('/folders/{id}/tasks/{task_id}/edit',[TaskController::class, 'showEditForm'])->name('tasks.edit');
-Route::post('/folders/{id}/tasks/{task_id}/edit',[TaskController::class, 'edit']);
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
